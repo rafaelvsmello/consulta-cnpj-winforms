6ae954abf6513022c223e5f092802b3016391551
rafaelvsmello <rmellov@gmail.com>
2021-02-24 16:07:46 -0300

    Add project files.

---
+++ ConsultaCnpj/Form.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ConsultaCnpj
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.txtCnpj = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lstDados = new System.Windows.Forms.ListBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConsultar);
+            this.groupBox1.Controls.Add(this.txtCnpj);
+            this.groupBox1.Location = new System.Drawing.Point(14, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(458, 105);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CNPJ";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.DimGray;
+            this.btnConsultar.FlatAppearance.BorderSize = 0;
+            this.btnConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsultar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.ForeColor = System.Drawing.Color.White;
+            this.btnConsultar.Location = new System.Drawing.Point(291, 48);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(119, 25);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // txtCnpj
+            // 
+            this.txtCnpj.Location = new System.Drawing.Point(20, 48);
+            this.txtCnpj.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCnpj.MaxLength = 20;
+            this.txtCnpj.Name = "txtCnpj";
+            this.txtCnpj.Size = new System.Drawing.Size(265, 25);
+            this.txtCnpj.TabIndex = 0;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnCadastrar.FlatAppearance.BorderSize = 0;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.Color.White;
+            this.btnCadastrar.Location = new System.Drawing.Point(362, 469);
+            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(110, 34);
+            this.btnCadastrar.TabIndex = 2;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // lstDados
+            // 
+            this.lstDados.FormattingEnabled = true;
+            this.lstDados.ItemHeight = 17;
+            this.lstDados.Location = new System.Drawing.Point(14, 135);
+            this.lstDados.Name = "lstDados";
+            this.lstDados.Size = new System.Drawing.Size(458, 327);
+            this.lstDados.TabIndex = 4;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnLimpar.FlatAppearance.BorderSize = 0;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(246, 469);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(110, 34);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(500, 516);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lstDados);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta CNPJ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCnpj;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.ListBox lstDados;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ ConsultaCnpj/Form.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace ConsultaCnpj
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private void CarregarDados(string cnpj, string cnpjTratado)
+        {
+            try
+            {
+                var requisicao = WebRequest.CreateHttp("https://www.receitaws.com.br/v1/cnpj/" + cnpjTratado);
+                requisicao.Method = "GET";
+                requisicao.UserAgent = "RequisicaoEmpresa";
+
+                using (var response = requisicao.GetResponse())
+                {
+                    var streamDados = response.GetResponseStream();
+                    var streamReader = new StreamReader(streamDados);
+                    var objResponse = streamReader.ReadToEnd();
+
+                    var responseDeserialized = JsonConvert.DeserializeObject<Empresa.Root>(objResponse.ToString());
+
+                    lstDados.Items.Add("CNPJ: " + cnpj);
+                    lstDados.Items.Add("Nome Empresarial: " + responseDeserialized.nome);
+                    lstDados.Items.Add(string.IsNullOrEmpty(responseDeserialized.fantasia) ? "SEM NOME FANTASIA" : "Nome Fantasia: " + responseDeserialized.fantasia);
+                    lstDados.Items.Add("Endereço: " + responseDeserialized.logradouro + " Nº: " + responseDeserialized.numero);
+                    lstDados.Items.Add(string.IsNullOrEmpty(responseDeserialized.complemento) ? "SEM COMPLEMENTO" : "Complemento: " + responseDeserialized.complemento);
+                    lstDados.Items.Add("Bairro: " + responseDeserialized.bairro);
+                    lstDados.Items.Add("Cidade: " + responseDeserialized.municipio);
+                    lstDados.Items.Add("CEP: " + responseDeserialized.cep);
+                    lstDados.Items.Add("Abertura: " + responseDeserialized.abertura);
+
+                    streamDados.Close();
+                    streamReader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+            }            
+        }
+
+        private void GravarDados()
+        { }
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCnpj.Text))
+            {
+                MessageBox.Show("Informe um CNPJ para pesquisar.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                CarregarDados(txtCnpj.Text, txtCnpj.Text.Replace(".", "").Replace("/", "").Replace("-", ""));
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtCnpj.Text = string.Empty;
+            lstDados.Items.Clear();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (lstDados.Items.Count > 0)
+            {
+                GravarDados();
+            }
+            else
+            {
+                MessageBox.Show("Informe um CNPJ para pesquisar e depois cadastrar.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+    }
+}
